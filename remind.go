1825bb854420943ffd1074d33af8bb2089b0748a
Jamie Flint <jamie@cydonia.co>
2013-09-06 18:13:01 +0100

    Added reason message

--- remind.go
+++ remind.go
@@ -3,12 +3,13 @@ package main
 import (
 	"github.com/jdiez17/irc-go"
 	"strconv"
+	"strings"
 	"time"
 )
 
 func remindCommandHandler(c *irc.Connection, e *irc.Event) {
 	if len(e.Params) < 1 {
-		e.React(c, "Not enough parameters! (syntax: .remind <time in minutes>)")
+		e.React(c, "Not enough parameters! (syntax: .remind <time in minutes> <reason>)")
 		return
 	}
 	inputTimeS := e.Params[0]
@@ -18,6 +19,6 @@ func remindCommandHandler(c *irc.Connection, e *irc.Event) {
 		return
 	}
 	time.Sleep(time.Duration(inputTime) * time.Minute)
-	message := "This is  your " + inputTimeS + " minute reminder"
+	message := "This is your " + inputTimeS + " minute reminder " + strings.Join(e.Params[1:], " ")
 	e.React(c, message)
 }
